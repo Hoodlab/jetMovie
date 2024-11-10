f5e39b6f3222e0e9f9e37d68cce6a21dcd5f6491
hood <hoods.tech@gmail.com>
2024-11-10 17:39:28 +0300

    development

---
+++ app/src/main/java/com/example/jetmovie/movie/data/remote/api/MovieApiService.kt
@@ -0,0 +1,22 @@
+package com.example.jetmovie.movie.data.remote.api
+
+import com.example.jetmovie.BuildConfig
+import com.example.jetmovie.movie.data.remote.models.MovieDto
+import com.example.jetmovie.utils.K
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApiService {
+
+    @GET(K.MOVIE_ENDPOINT)
+    suspend fun fetchDiscoverMovie(
+        @Query("api_key") apiKey: String = BuildConfig.apiKey,
+        @Query("include_adult") includeAdult: Boolean = false
+    ): MovieDto
+
+    @GET(K.TRENDING_MOVIE_ENDPOINT)
+    suspend fun fetchTrendingMovie(
+        @Query("api_key") apiKey: String = BuildConfig.apiKey,
+        @Query("include_adult") includeAdult: Boolean = false
+    ): MovieDto
+}
---
+++ app/src/main/java/com/example/jetmovie/ui/navigation/Route.kt
@@ -0,0 +1,15 @@
+package com.example.jetmovie.ui.navigation
+
+import com.example.jetmovie.utils.K
+
+sealed class Route {
+    data class HomeScreen(val route: String = "homeScreen") : Route()
+    data class FilmScreen(
+        val route: String = "FilmScreen",
+        val routeWithArgs: String = "$route/{${K.MOVIE_ID}}",
+    ) : Route() {
+        fun getRouteWithArgs(id: Int): String {
+            return "$route/$id"
+        }
+    }
+}
